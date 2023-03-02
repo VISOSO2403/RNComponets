227d757a8d61db90969012d93689267547541935
Oliver Visoso <ovisoso@ucol.mx>
2023-03-01 19:54:29 -0600

    Animaciones

---
+++ src/screens/Animation102Screen.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native';
+
+const Animation102Screen = () => {
+  return (
+    <View style={ styles.container }>
+      <View style={styles.purpleBox} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  purpleBox: {
+    backgroundColor: '#75CEDB',
+    width: 150,
+    height: 150
+  }
+})
+
+export default Animation102Screen
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { FlatList, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import { styles } from '../theme/appTheme';
+import { MenuItem } from '../interfaces/appInterfaces';
+import FlatListMenuItem from '../components/FlatListMenuItem';
+
+
+const menuItems: MenuItem[] = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen'
+  },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    component: 'Animation102Screen'
+  }
+]
+
+const HomeScreen = () => {
+
+  const { top } = useSafeAreaInsets()
+
+  const renderMenuItem = ( menuItem: MenuItem) => {
+    
+  }
+
+  const renderListHeader = () => {
+    return (
+      <View style={{ marginTop: top + 20, marginBottom: 20}}>
+        <Text style={styles.title}>Opciones de Menu</Text>
+      </View>
+    )
+  }
+  
+  const itemSeparator = () => {
+    return (
+      <View 
+        style={{
+          borderBottomWidth: 1,
+          opacity: 0.4,
+          marginVertical: 8
+        }}
+      />
+    )
+  }
+
+  return (
+    <View style={{ flex: 1, ...styles.globalMargin}}>
+      <FlatList 
+        data={ menuItems }
+        renderItem={ ({ item }) => <FlatListMenuItem menuItem={ item }/> }
+        keyExtractor={ (item) => item.name }
+        ListHeaderComponent={ renderListHeader }
+        ItemSeparatorComponent={ itemSeparator }
+      />
+    </View>
+  )
+}
+
+export default HomeScreen
