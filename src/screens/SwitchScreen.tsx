31eca0e1e2f0fa9aff4ab5017feeec78caffa81d
Oliver Visoso <ovisoso@ucol.mx>
2023-06-17 22:24:12 -0600

    Component Switch

--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Switch, Platform, StyleSheet} from 'react-native';
+import HeaderTitle from '../components/HeaderTitle';
+import CustomSwitch from '../components/CustomSwitch';
 
 const SwitchScreen = () => {
+  const [first, setFirst] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
+  const {isActive, isHungry, isHappy} = first;
+
+  const onChange = (value: boolean, field: string) => {
+    setFirst({
+      ...first,
+      [field]: value,
+    });
+  };
+
   return (
-    <View>
-      <Text>SwitchScreen</Text>
+    <View style={{marginHorizontal: 20}}>
+      <HeaderTitle title="Switches" />
+
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isActive</Text>
+        <CustomSwitch
+          isOn={isActive}
+          onChange={value => onChange(value, 'isActive')}
+        />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHungry</Text>
+        <CustomSwitch
+          isOn={isHungry}
+          onChange={value => onChange(value, 'isHungry')}
+        />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHappy</Text>
+        <CustomSwitch
+          isOn={isHappy}
+          onChange={value => onChange(value, 'isHappy')}
+        />
+      </View>
+
+      <Text style={styles.switchText}>{JSON.stringify(first, null, 5)}</Text>
     </View>
   );
 };
+
+export const styles = StyleSheet.create({
+  switchRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+  switchText: {
+    fontSize: 25,
+  },
+});
 
 export default SwitchScreen;
